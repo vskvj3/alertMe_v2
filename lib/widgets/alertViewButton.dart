c87659ddc09dd6cb0d28e22dc133827dc7ce2e40
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-05-24 20:18:53 +0530

    changed FlatButton to Textbutton
    FlatButton is depricated.
    changed it to TextButton.
    also removed padding which is not supported in TextButton

--- lib/widgets/alertViewButton.dart
+++ lib/widgets/alertViewButton.dart
@@ -2,13 +2,12 @@ import "package:flutter/material.dart";
 
 Widget alertViewButton(String buttonText, void Function() buttonAction,
     Color buttonColor, FontWeight buttonFontWeight) {
-  return FlatButton(
+  return TextButton(
     onPressed: buttonAction,
     child: Text(buttonText,
         style: TextStyle(
           color: buttonColor,
           fontSize: 28,
         )),
-    padding: EdgeInsets.only(top: 18, bottom: 20),
   );
 }
