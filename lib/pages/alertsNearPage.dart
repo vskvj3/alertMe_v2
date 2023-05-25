f085896c765e5775ecb7e05e06af03e3770fb999
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-05-25 23:56:57 +0530

    refactor: removed unused libraries

--- lib/pages/alertsNearPage.dart
+++ lib/pages/alertsNearPage.dart
@@ -1,25 +1,20 @@
 import 'package:AlertMe/widgets/AlertListField.dart';
 import 'package:flutter/material.dart';
-import 'package:AlertMe/homepage.dart';
-import 'package:AlertMe/widgets/alertViewButton.dart';
 import 'package:AlertMe/widgets/AlertViewNavigationBar.dart';
 
 class AlertsNear extends StatelessWidget {
-  final Widget verticalDivider =
-      VerticalDivider(thickness: 1, color: Colors.grey);
-
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(170, 219, 253, 1),
-        title: Text('Alerts Recieved',
+        title: const Text('Alerts Recieved',
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
       ),
       body: Container(
           padding: EdgeInsets.only(left: 2, top: 50, right: 2),
           child: ListView(
-            children: [
+            children: const [
               //will appear only if an alert is recieved
 
               alertListField(
