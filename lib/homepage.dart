1f855da0616bf57a806b12b9a657adc96ebfd272
Abin-Augustine10 <abinaugustine1010@gmail.com>
2023-05-25 22:44:03 +0530

    copy pasted from Abhishek

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 
+import 'dart:async';
+
 import 'package:AlertMe/pages/EmergencyProfilePage.dart';
 import 'package:AlertMe/pages/alertsNearPage.dart';
 import 'package:flutter/material.dart';
@@ -9,128 +11,128 @@ import 'package:AlertMe/pages/FAQpage.dart';
 import 'package:AlertMe/pages/EmergencyContactsPage.dart';
 import 'package:AlertMe/pages/SettingsPage.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-class HomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
+  bool isVisible = true;
+  static const maxSeconds = 5;
+  int seconds = maxSeconds;
+  Timer? timer;
 
-  Widget verticalDivider=VerticalDivider(
-    thickness: 1,
-    color:Colors.grey
-  );
-
-
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      if (seconds > 0) {
+        setState(() {
+          seconds--;
+        });
+      } else {
+        setState(() {
+          timer?.cancel();
+          seconds = maxSeconds;
+          isVisible = !isVisible;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child:Scaffold(
-
-      //navbar 
-      
-      appBar: AppBar(
-        
-        backgroundColor: Color.fromRGBO(170, 219, 253, 1),
-          title: Text('AlertMe',style: 
-          TextStyle(color:Colors.black,
-          fontWeight: FontWeight.bold)),
-          
-          
+    return SafeArea(
+      child: Scaffold(
+//AppBar of home page
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(170, 219, 253, 1),
+          title: const Text('AlertMe',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
           actions: [
             PopupMenuButton<String>(
-              color:Color.fromRGBO(170, 219, 253, 1),
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              color: const Color.fromRGBO(170, 213, 253, 1),
               onSelected: (String choice) {
                 // Handle menu item selection
 
                 if (choice == 'Settings') {
                   Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SettingsPage(),
-              ),
-            );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SettingsPage(),
+                    ),
+                  );
                   print('Settings Page');
-                } 
-                
-                
-                else if (choice == 'Emergency Profile') {
+                } else if (choice == 'Emergency Profile') {
                   Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => EmergencyProfilePage(),
-              ),
-            );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EmergencyProfilePage(),
+                    ),
+                  );
                   print('Emergency Profile Page');
-                }
-
-
-                 else if (choice == 'Emergency Contacts') {
+                } else if (choice == 'Emergency Contacts') {
                   Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => EmergencyContactsPage(),
-              ),
-            );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EmergencyContactsPage(),
+                    ),
+                  );
                   print('Emergency Contacts Page');
-                 }
-                
-                
-                else if (choice == 'FAQ') {
+                } else if (choice == 'FAQ') {
                   Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => FAQPage(),
-              ),
-            );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FAQPage(),
+                    ),
+                  );
                   print('FAQ Page');
-                }
-                
-                
-                else if (choice == 'About') {
+                } else if (choice == 'About') {
                   Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AboutPage(),
-              ),
-            );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AboutPage(),
+                    ),
+                  );
                   print('About Page');
-                }
-                
-                
-                else if (choice == 'Quit') {
-                   SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                } else if (choice == 'Quit') {
+                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
                   print('Quit');
                 }
               },
-
-
               itemBuilder: (BuildContext context) {
-                
                 return [
-                  PopupMenuItem<String>(
+                  const PopupMenuItem<String>(
                     value: 'Settings',
                     child: Text('Settings'),
                   ),
-                  PopupMenuDivider(height:20),
-                  PopupMenuItem<String>(
+                  const PopupMenuDivider(height: 20),
+                  const PopupMenuItem<String>(
                     value: 'Emergency Profile',
                     child: Text('Emergency Profile'),
                   ),
-                  PopupMenuDivider(height:20),
-                  PopupMenuItem<String>(
+                  const PopupMenuDivider(height: 20),
+                  const PopupMenuItem<String>(
                     value: 'Emergency Contacts',
                     child: Text('Emergency Contacts'),
                   ),
-                  PopupMenuDivider(height:20),
-                  PopupMenuItem<String>(
+                  const PopupMenuDivider(height: 20),
+                  const PopupMenuItem<String>(
                     value: 'FAQ',
                     child: Text('FAQ'),
                   ),
-                  PopupMenuDivider(height:20),
-                  PopupMenuItem<String>(
+                  const PopupMenuDivider(height: 20),
+                  const PopupMenuItem<String>(
                     value: 'About',
                     child: Text('About'),
                   ),
-                  PopupMenuDivider(height:20),
-                  PopupMenuItem<String>(
+                  const PopupMenuDivider(height: 20),
+                  const PopupMenuItem<String>(
                     value: 'Quit',
                     child: Text('Quit'),
                   ),
@@ -138,139 +140,177 @@ class HomePage extends StatelessWidget {
               },
             ),
           ],
-
-          
         ),
-
-
 
 // body of home page
 
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 110.0,
+            ),
+            Visibility(
+              visible: isVisible,
+              child: Container(
+                color: Colors.white,
+                height: 293.0,
+                padding: const EdgeInsets.only(),
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    startTimer();
+                    setState(() {
+                      isVisible = !isVisible;
+                    });
 
-
-      body:Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(
-            height:110.0,
-          ),
-
-
-
-          Container(
-            color:Colors.white,
-            height:293.0,
-            padding: EdgeInsets.only(),
-            alignment: Alignment.center,
-            child: ElevatedButton(   
-                      
-              onPressed: () {
-                print("ALERT!!");
-                // Handle panic button press event
-              },
-              child: Text(
-                'ALERT',
-                style: TextStyle(
-                  fontSize: 35.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              style: ButtonStyle(
-
-                
-                shape: MaterialStateProperty.all<CircleBorder>(
-                  CircleBorder(),
-                ),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  EdgeInsets.all(120),
-                ),
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Colors.red,
-                ),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  Colors.white,
-                ),
-                overlayColor: MaterialStateProperty.all<Color>(
-                  Colors.red[800]!,
-                ),
-                elevation: MaterialStateProperty.all<double>(10.0),
-                shadowColor: MaterialStateProperty.all<Color>(
-                  Colors.red[800]!,
-                ),
-                animationDuration: Duration(milliseconds: 200),
-                textStyle: MaterialStateProperty.all<TextStyle>(
-                  TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5,
+                    // Handle panic button press event
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<CircleBorder>(
+                      const CircleBorder(),
+                    ),
+                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      const EdgeInsets.all(120),
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.red,
+                    ),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                      Colors.white,
+                    ),
+                    overlayColor: MaterialStateProperty.all<Color>(
+                      Colors.red[800]!,
+                    ),
+                    elevation: MaterialStateProperty.all<double>(10.0),
+                    shadowColor: MaterialStateProperty.all<Color>(
+                      Colors.red[800]!,
+                    ),
+                    animationDuration: const Duration(milliseconds: 200),
+                    textStyle: MaterialStateProperty.all<TextStyle>(
+                      const TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'ALERT',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          
-        ],
-        
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child:Container(
-          height:75,
-          color: Color(0xFFAADBFD),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              alertViewButton('Alert', 
-              (){
+            Visibility(
+              visible: !isVisible,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 110.0,
+                    ),
+                    const Text(
+                      'Sending Alerts in',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          CircularProgressIndicator(
+                            value:(seconds / maxSeconds),
+                            valueColor: const AlwaysStoppedAnimation(Colors.red),
+                            strokeWidth: 12,
+                          ),
+                          Center(
+                              child: Text(
+                            '$seconds',
+                            style: const TextStyle(
+                              fontSize: 40.0,
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 200,
+                      height:60,
+                      child:ElevatedButton(
+                        style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(Color.fromRGBO(128, 0, 0, 1.0)),
+                          foregroundColor: MaterialStatePropertyAll(Colors.white),
+                        ),
+                      onPressed: () {
+                        setState(() {
+                          timer?.cancel();
+                          seconds = maxSeconds;
+                          isVisible = !isVisible;
+                        });
+                      },
+                      child: const Text(
+                        'Cancel',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 75,
+            color: Color(0xFFAADBFD),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                alertViewButton('Alert', () {
                   // what happens when the alert button is clicked
                   Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(
-                builder: (context) => HomePage(),
-              ),
-              (route) => false, // Prevent navigating back
-            );
-                }, 
-                 Colors.red,
-                 FontWeight.w400),
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                    (route) => false, // Prevent navigating back
+                  );
+                }, Colors.red, FontWeight.w400),
 
-              // Text(
-              //   'Alert',
-              //   style: TextStyle(
-              //     fontSize: 28.0,
-              //     fontWeight: FontWeight.w300,
-              //     color: Colors.red,
-              //   ),
-              // ),
-              verticalDivider,
-              // Text(
-              //   'View',
-              //   style: TextStyle(
-              //     fontSize: 28.0,
-              //     fontWeight: FontWeight.w300,
-              //     color: Colors.black,
-              //   ),
-              // ),
-              alertViewButton('View',
-              (){
+                
+                alertViewButton('View', () {
                   Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AlertsNear(),
-              ),
-              (route) => false, // Prevent navigating back
-            );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AlertsNear(),
+                    ),
+                    (route) => false, // Prevent navigating back
+                  );
                   print('Alert View Page');
-                }, 
-                 Colors.black,
-                 FontWeight.w400),
-            ],
+                }, Colors.black, FontWeight.w400),
+              ],
+            ),
           ),
         ),
-        
-
       ),
-    
-    ));
+    );
   }
 }
