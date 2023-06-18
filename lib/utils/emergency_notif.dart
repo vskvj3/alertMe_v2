d27cfa20c275ee1599ca84887601f510b909dbd1
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-06-18 14:51:17 +0530

    fix: icon error in notification

--- lib/utils/emergency_notif.dart
+++ lib/utils/emergency_notif.dart
@@ -1,32 +1,31 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class AppNotif{
+class AppNotif {
+  final appNotif = FlutterLocalNotificationsPlugin();
 
-      final appNotif = FlutterLocalNotificationsPlugin();
-
-      Future<void> initNotification() async{
-        AndroidInitializationSettings initializationSettingsAndroid = const AndroidInitializationSettings('flutter_icon');
-        InitializationSettings initializationSettings = InitializationSettings(android:initializationSettingsAndroid);
-        await appNotif.initialize(initializationSettings,
+  Future<void> initNotification() async {
+    AndroidInitializationSettings initializationSettingsAndroid =
+        const AndroidInitializationSettings('mipmap/ic_launcher');
+    InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await appNotif.initialize(initializationSettings,
         onDidReceiveBackgroundNotificationResponse:
-        ( NotificationResponse notificationResponse) async {});
-      }
-      
-      notificationDetails(){
-        return const NotificationDetails(
-          android: AndroidNotificationDetails('1', '2',importance: Importance.max,icon:'flutter_icon',ongoing: true,autoCancel: false, visibility:NotificationVisibility.public),
-        );
-      }
-
-      Future showNotification(
-        {int id = 0,String? title,String? body,String? payload})async{
-
-          return appNotif.show(id, title, body, await notificationDetails());
-      }
-
-
+            (NotificationResponse notificationResponse) async {});
   }
 
+  notificationDetails() {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails('1', '2',
+          importance: Importance.max,
+          icon: 'mipmap/ic_launcher',
+          ongoing: true,
+          autoCancel: false,
+          visibility: NotificationVisibility.public),
+    );
+  }
 
-  
-
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payload}) async {
+    return appNotif.show(id, title, body, await notificationDetails());
+  }
+}
