2278aac9917fd1e5de8e45fb502cededf1be59df
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-06-19 22:00:48 +0530

    feat: add distance in nearby alerts

--- lib/pages/nearby_alerts.dart
+++ lib/pages/nearby_alerts.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:alert_me/utils/alert_receiver.dart';
+import 'package:alert_me/utils/location_finder.dart';
 import 'package:alert_me/widgets/alert_list_field.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class AlertsNear extends StatefulWidget {
   const AlertsNear({Key? key}) : super(key: key);
@@ -26,30 +30,68 @@ class _AlertsNearState extends State<AlertsNear> {
     });
   }
 
+  Future<String> findDistance(String remoteLocation) async {
+    final Position currentLocation = await LocationModule.determinePosition();
+    debugPrint('${currentLocation.latitude}');
+    final List<dynamic> locations = json.decode(remoteLocation);
+    print("locations: ${locations[0].toDouble()}");
+    print("locations: ${locations[1]}");
+    String distance = LocationModule.calculateDistance(
+        locations[0].toDouble(),
+        locations[1].toDouble(),
+        currentLocation.latitude.toDouble(),
+        currentLocation.longitude.toDouble());
+    debugPrint('distance: $distance');
+    // Calculate the distance using the current location and the remote location
+    // Replace the return statement with your distance calculation logic
+    return distance;
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: categories,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return SingleChildScrollView(
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: alertDataList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return AlertListField(
-                    distance: alertDataList[index].location,
-                    nearFar: "to be done",
-                    name: alertDataList[index].name,
-                    alertDetails: alertDataList[index],
-                  );
-                },
-              ),
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        });
+      future: categories,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return SingleChildScrollView(
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: alertDataList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return FutureBuilder(
+                  future: findDistance(alertDataList[index].location),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      final distance = snapshot.data as String;
+                      return AlertListField(
+                        distance: distance,
+                        nearFar: "flagged: ${alertDataList[index].flagCount}",
+                        name: alertDataList[index].name,
+                        alertDetails: alertDataList[index],
+                      );
+                    } else {
+                      return const Column(
+                        children: [
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Center(child: CircularProgressIndicator()),
+                          SizedBox(
+                            height: 5,
+                          )
+                        ],
+                      );
+                    }
+                  },
+                );
+              },
+            ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
--- lib/utils/location_finder.dart
+++ lib/utils/location_finder.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:geolocator/geolocator.dart';
 
 class LocationModule {
@@ -55,12 +54,18 @@ class LocationModule {
     }
   }
 
-  double calculateDistance(lat1, lon1, lat2, lon2) {
+  static String calculateDistance(lat1, lon1, lat2, lon2) {
     var p = 0.017453292519943295;
     var c = cos;
     var a = 0.5 -
         c((lat2 - lat1) * p) / 2 +
         c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a));
+    var distInKm = 12742 * asin(sqrt(a));
+    if (distInKm < 1) {
+      distInKm *= 1000;
+      return '${distInKm.toInt()}m away';
+    } else {
+      return '${distInKm.toStringAsFixed(2)}Km away';
+    }
   }
 }
